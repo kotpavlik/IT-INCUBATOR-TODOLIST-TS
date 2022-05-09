ae27753e55fd601d346f1407ee4e816bd6517419
kotpavlik <kotpavlik88@gmail.com>
2022-05-09 16:10:57 +0300

    localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from './components/todo_list/TodoList';
 import {v1} from 'uuid';
-import {AddItemForm} from "./components/addItemForm/AddItemForm";
+import {AddItemForm} from './components/addItemForm/AddItemForm';
 import {ExampleAnimation} from './components/lottie/LottieAnimation';
-
 
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
@@ -22,37 +21,61 @@ function App() {
     const tasksID_1 = v1();
     const tasksID_2 = v1();
 
-    let [todoLists, setTodoLists] = useState<Array<TodoListsType>>([
-        {id: tasksID_1, title: 'What learn', filter: 'all'},
-        {id: tasksID_2, title: 'What buy', filter: 'all'}]);
+    let [todoLists, setTodoLists] = useState<Array<TodoListsType>>(
+        () => {
+            let newValue_todoLists = localStorage.getItem('value_todoLists');
+            if (newValue_todoLists) {
+                let new_start_value = JSON.parse(newValue_todoLists);
+                return new_start_value ||
+                    [{id: tasksID_1, title: 'What learn', filter: 'all'},
+                        {id: tasksID_2, title: 'What buy', filter: 'all'}];
+            }
+        });
     let [tasksObj, setTasksObj] = useState<TasksObjType>(
-        {
-            [tasksID_1]:[
-                {id: v1(), title: 'HTML', isDone: true},
-                {id: v1(), title: 'CSS', isDone: false},
-                {id: v1(), title: 'JS', isDone: true},
-                {id: v1(), title: 'JS', isDone: true},
-                {id: v1(), title: 'JS', isDone: true},
-            ],
-            [tasksID_2]:[
-                {id: v1(), title: 'Bread', isDone: true},
-                {id: v1(), title: 'Milk', isDone: false}
-            ]
-        })
+        () => {
+            let newValue_tasksObj = localStorage.getItem('value_tasks');
+            if (newValue_tasksObj) {
+                let new_start_value = JSON.parse(newValue_tasksObj);
+                return new_start_value ||
+                    {
+                        [tasksID_1]: [
+                            {id: v1(), title: 'HTML', isDone: true},
+                            {id: v1(), title: 'CSS', isDone: false},
+                            {id: v1(), title: 'JS', isDone: true},
+                            {id: v1(), title: 'JS', isDone: true},
+                            {id: v1(), title: 'JS', isDone: true},
+                        ],
+                        [tasksID_2]: [
+                            {id: v1(), title: 'Bread', isDone: true},
+                            {id: v1(), title: 'Milk', isDone: false}
+                        ]
+                    }
+            }
+        }
+    );
+
+    useEffect(() => {
+        localStorage.setItem('value_todoLists', JSON.stringify(todoLists))
+    }, [todoLists]);
+    useEffect(() => {
+        localStorage.setItem('value_tasks', JSON.stringify(tasksObj))
+    }, [tasksObj]);
 
     function removeTasks(id: string, todoListId: string) {
         let tasks = tasksObj[todoListId];
         let filteredTasks = tasks.filter((el) => el.id !== id);
-         tasksObj[todoListId] = filteredTasks;
+        tasksObj[todoListId] = filteredTasks;
         setTasksObj({...tasksObj});
     }
-    function addTasks(taskName: string,todoListId: string) {
+
+    function addTasks(taskName: string, todoListId: string) {
         let tasks = tasksObj[todoListId];
         let task: TaskType = {id: v1(), title: taskName, isDone: false};
         let newTask = [...tasks, task]
         tasksObj[todoListId] = newTask;
         setTasksObj({...tasksObj});
     }
+
     function changeFilter(value: FilterValuesType, todoListId: string) {
         setTodoLists(todoLists.map(el => el.id === todoListId ? {...el, filter: value} : el))
         // setTodoLists(todoLists.map(tl => {
@@ -62,7 +85,8 @@ function App() {
         //     return tl;
         // }))
     }
-    function changeIsDoneTask(taskId: string, isDone: boolean,todoListId: string) {
+
+    function changeIsDoneTask(taskId: string, isDone: boolean, todoListId: string) {
         // let tasks = tasksObj[todoListId];
         // let task = tasks.find(t => t.id === taskId);
         // if (task) {
@@ -70,29 +94,38 @@ function App() {
         // }
         // tasksObj[todoListId] = tasks;
         // setTasksObj({...tasksObj})
-        setTasksObj({...tasksObj,[todoListId]:tasksObj[todoListId].map(el => el.id === taskId ? {...el,isDone} : el )})
+        setTasksObj({
+            ...tasksObj,
+            [todoListId]: tasksObj[todoListId].map(el => el.id === taskId ? {...el, isDone} : el)
+        })
     }
-    function removeTodoList  (todoListId:string)  {
-        let newTodoList = todoLists.filter( td => td.id !== todoListId )
+
+    function removeTodoList(todoListId: string) {
+        let newTodoList = todoLists.filter(td => td.id !== todoListId)
         setTodoLists(newTodoList);
         delete tasksObj[todoListId];
         setTasksObj({...tasksObj});
     }
-    function addNewTodoLists (title:string) {
-    let newTodoList:TodoListsType = {id:v1(), title, filter:'all'};
-    let addNewTodolist = [...todoLists, newTodoList];
-    let addNewTasksObj = {...tasksObj, [newTodoList.id]:[]};
-    setTodoLists(addNewTodolist);
-    setTasksObj(addNewTasksObj);
+
+    function addNewTodoLists(title: string) {
+        let newTodoList: TodoListsType = {id: v1(), title, filter: 'all'};
+        let addNewTodolist = [...todoLists, newTodoList];
+        let addNewTasksObj = {...tasksObj, [newTodoList.id]: []};
+        setTodoLists(addNewTodolist);
+        setTasksObj(addNewTasksObj);
     }
+
     function renameTodoList(newTitle: string, todoListId: string) {
-        setTodoLists(todoLists.map(el => el.id === todoListId ? {...el, title : newTitle} : el))
-    }
-    function renameTasks(newTitle:string, taskId:string, todoListId:string) {
-        setTasksObj({...tasksObj,[todoListId]:tasksObj[todoListId].map(el => el.id === taskId ? {...el, title : newTitle} : el)})
-
+        setTodoLists(todoLists.map(el => el.id === todoListId ? {...el, title: newTitle} : el))
     }
 
+    function renameTasks(newTitle: string, taskId: string, todoListId: string) {
+        setTasksObj({
+            ...tasksObj,
+            [todoListId]: tasksObj[todoListId].map(el => el.id === taskId ? {...el, title: newTitle} : el)
+        })
+
+    }
 
 
     return (
