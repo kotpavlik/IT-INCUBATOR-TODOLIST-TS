1b95200977131d09ae24c273fe64cfd6969cf702
kotpavlik <kotpavlik88@gmail.com>
2022-05-09 18:28:02 +0300

    refactor

--- src/components/todo_list/TodoList.tsx
+++ src/components/todo_list/TodoList.tsx
@@ -7,8 +7,7 @@ import EditableSpan from '../editableSpan/EditableSpan.';
 import {IconButton} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Checkbox from '@mui/material/Checkbox';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
