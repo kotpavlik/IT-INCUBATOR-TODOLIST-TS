aa4197325e1317b961fe69d2f5492c18f0c641f9
kotpavlik <kotpavlik88@gmail.com>
2022-05-09 16:24:00 +0300

    Updates

---
+++ precache-manifest.e2c160ed6fb7205e4dba5f86d882873e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23fe16cf6358da038bd3a8d2d89477c",
+    "url": "/IT-INCUBATOR-TODOLIST-TS/index.html"
+  },
+  {
+    "revision": "f51c3944b2ce373e72e1",
+    "url": "/IT-INCUBATOR-TODOLIST-TS/static/css/main.9dd9daa5.chunk.css"
+  },
+  {
+    "revision": "1be98e08c1b0dc8c8463",
+    "url": "/IT-INCUBATOR-TODOLIST-TS/static/js/2.a445cd6c.chunk.js"
+  },
+  {
+    "revision": "6131c4308ad4aa04cab1861860b76712",
+    "url": "/IT-INCUBATOR-TODOLIST-TS/static/js/2.a445cd6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51c3944b2ce373e72e1",
+    "url": "/IT-INCUBATOR-TODOLIST-TS/static/js/main.1ac85550.chunk.js"
+  },
+  {
+    "revision": "f1c67bd6f344a95cac43",
+    "url": "/IT-INCUBATOR-TODOLIST-TS/static/js/runtime-main.2e5dee25.js"
+  }
+]);
