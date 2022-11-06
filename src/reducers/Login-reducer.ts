67eef764649d12fa113768f37bf98841ee367874
kotpavlik <kotpavlik88@gmail.com>
2022-11-06 13:58:20 +0300

    refactoring stor. go to the redux toolkot

--- src/reducers/Login-reducer.ts
+++ src/reducers/Login-reducer.ts
@@ -2,6 +2,7 @@ import {loginApi, LoginType, securityAPI} from '../api/API';
 import {AppThunk} from './store';
 import {setErrorApp, setStatusApp} from './App-reducer';
 import {Dispatch} from 'redux';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 
 
@@ -18,57 +19,38 @@ const initialState: initialStateType = {
     rememberMe: false,
     captcha: ''
 }
-export type LoginActionType = setAuthUserACType | getCaptchaUrlSuccessType
+export type LoginActionType = setAuthUserDataType | getCaptchaUrlSuccessType
+const slice = createSlice({
+    name:'login',
+    initialState,
+    reducers: {
+        setAuthUserData(state,action:PayloadAction<{data: LoginType, userId: number, isAuth: boolean}>) {
+            state.id = action.payload.userId
+            state.isAuth = action.payload.isAuth
+            state.email = action.payload.data.email
+            state.password = action.payload.data.password
+            state.rememberMe = action.payload.data.rememberMe
+            state.captcha = action.payload.data.captcha
+        },
+        getCaptchaUrlSuccess(state,action:PayloadAction<{CaptchaUrl:string}>) {
+            state.captcha = action.payload.CaptchaUrl
+        },
 
-export const loginReducer = (state: initialStateType = initialState, action: LoginActionType): initialStateType => {
-    switch (action.type) {
-        case 'SET_AUTH_USER': {
-            return {
-                ...state,
-                id: action.payload.userId,
-                isAuth: action.payload.isAuth,
-                email: action.payload.data.email,
-                password: action.payload.data.password,
-                rememberMe: action.payload.data.rememberMe,
-                captcha: action.payload.data.captcha
-            }
-        }
-        case 'GET_CAPTCHA_URL': {
-            return {...state,captcha:action.payload.CaptchaUrl}
-        }
-        default:
-            return state;
     }
-}
+})
 
-export type setAuthUserACType = ReturnType<typeof setAuthUserData>
-export const setAuthUserData = (data: LoginType, userId: number, isAuth: boolean) => {
-    return {
-        type: 'SET_AUTH_USER',
-        payload: {
-            data,
-            userId,
-            isAuth
-        }
-    } as const
-}
+export const loginReducer = slice.reducer
+export const {setAuthUserData,getCaptchaUrlSuccess} = slice.actions
+export type setAuthUserDataType = ReturnType<typeof setAuthUserData>
+export type getCaptchaUrlSuccessType = ReturnType<typeof getCaptchaUrlSuccess>
 
-type getCaptchaUrlSuccessType = ReturnType<typeof getCaptchaUrlSuccess>
-export const getCaptchaUrlSuccess = (CaptchaUrl:string) => {
-    return {
-        type: 'GET_CAPTCHA_URL',
-        payload: {
-            CaptchaUrl
-        }
-    } as const
-}
 
 
 export const getCaptchaUrl = ():AppThunk =>
      async(dispatch:Dispatch) => {
         let response = await securityAPI.getCaptcha();
         let CaptchaUrl = response.data.url;
-        dispatch(getCaptchaUrlSuccess(CaptchaUrl))
+        dispatch(getCaptchaUrlSuccess({CaptchaUrl}))
     }
 
 
@@ -78,8 +60,8 @@ export const updateLoginTC = (data: LoginType): AppThunk =>
         try {
             const resp = await loginApi.login(data)
             if (resp.data.resultCode === 0) {
-                dispatch(setAuthUserData(data, resp.data.data.userId, true))
-                dispatch(getCaptchaUrlSuccess(''))
+                dispatch(setAuthUserData({data, userId:resp.data.data.userId, isAuth:true}))
+                dispatch(getCaptchaUrlSuccess({CaptchaUrl:''}))
                 dispatch(setStatusApp({status:'succeeded'}))
             } else {
                 dispatch(setErrorApp({error:resp.data.messages[0]}))
@@ -96,8 +78,9 @@ export const removeLoginTC = (): AppThunk =>
         dispatch(setStatusApp({status:'loading'}))
         try {
             const resp = await loginApi.logout()
+            const data = {email: '', password: '', rememberMe: false}
             if (resp.resultCode === 0) {
-                dispatch(setAuthUserData({email: '', password: '', rememberMe: false}, 0, false))
+                dispatch(setAuthUserData({data, userId:0, isAuth:false}))
                 dispatch(setStatusApp({status:'succeeded'}))
             } else {
                 dispatch(setErrorApp({error:resp.messages[0]}))
